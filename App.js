193cc473b75a90ced018d51dcee208fc19165b08
Kenneth Dimalibot <dimalibotkennethofficial@gmail.com>
2023-11-23 14:24:53 +0800

    sdad

--- App.js
+++ App.js
@@ -2,8 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+
   return (
-    <View style={styles.container}>
+    <View style={{backgroundColor: 'orange'}}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
